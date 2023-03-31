27b4ce03b1b8a70893ea9582262350b0e2b45564
Claudio Cicconetti <cicconetti@gmail.com>
2023-03-31 09:59:02 +0200

    Add BroMin/BroMax implementation

---
+++ allocation/algorithms.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+import math
+
+
+class ProblemInstance:
+    """Instance of a problem to be solved."""
+
+    def __init__(
+        self,
+        T: float = 100e6,
+        L: float = 200e-3,
+        U: float = 2000e-3,
+        Tp: float = 10e6,
+        Tc: float = 20e6,
+        Hmax: int = 30000,
+        lr: float = 1e-3,
+        u: float = 5e-3,
+    ):
+        """Create a problem instance and set the system parameters.
+
+        Args:
+            T (float, optional): Cluster throughput, in b/s. Defaults to 100e6.
+            L (float, optional): Replication latency threshould, in s. Defaults to 200e-3.
+            U (float, optional): Application unavailability threshold, in s. Defaults to 2000e-3.
+            Tp (float, optional): Max producer throughput, in b/s. Defaults to 10e6.
+            Tc (float, optional): Max consumer throughput, in b/s. Defaults to 20e6.
+            Hmax (int, optional): Max number of open file descriptors. Defaults to 30000.
+            lr (float, optional): Replication latency, in s. Defaults to 1e-3.
+            u (float, optional): Unobservability time, in s. Defaults to 5e-3.
+        """
+        assert T > 0
+        assert L > 0
+        assert U > 0
+        assert Tp > 0
+        assert Tc > 0
+        assert Hmax > 0
+        assert lr > 0
+        assert u > 0
+
+        self.T = T
+        self.L = L
+        self.U = U
+        self.Tp = Tp
+        self.Tc = Tc
+        self.Hmax = Hmax
+        self.lr = lr
+        self.u = u
+
+    def broMin(
+        self,
+        c: int,
+        r: int,
+        B: int,
+    ):
+        """Select the number of partitions/brokers with BroMin.
+
+        Args:
+            c (int): Number of consumers.
+            r (int): Replication factor.
+            B (int): Number of brokers available.
+
+        Returns:
+            tuple[int, int]: number of partitions (P) and brokers used (b), or (-1,-1) if there is no feasible solution.
+        """
+
+        assert c > 0
+        assert r > 0
+        assert B > 0
+
+        for b in range(r, B + 1, 1):
+            for P in range(self.maxP(b, r), self.minP(c) - 1, -1):
+                if self.check_feasible(r, P, b):
+                    return (P, b)
+
+        return (-1, -1)
+
+    def broMax(
+        self,
+        c: int,
+        r: int,
+        B: int,
+    ):
+        """Select the number of partitions/brokers with BroMax.
+
+        Args:
+            c (int): Number of consumers.
+            r (int): Replication factor.
+            B (int): Number of brokers available.
+
+        Returns:
+            tuple[int, int]: number of partitions (P) and brokers used (b), or (-1,-1) if there is no feasible solution.
+        """
+
+        assert c > 0
+        assert r > 0
+        assert B > 0
+
+        for b in range(B, r - 1, -1):
+            for P in range(self.maxP(b, r), self.minP(c) - 1, -1):
+                if self.check_feasible(r, P, b):
+                    return (P, b)
+
+        return (-1, -1)
+
+    def maxP(self, b: int, r: int):
+        assert b > 0
+        assert r > 0
+        return int(math.floor(b * self.Hmax / r))
+
+    def minP(self, c: int):
+        assert c > 0
+        return int(max(int(round(self.T / self.Tp)), int(round(self.T / self.Tc)), c))
+
+    def check_feasible(self, r: int, P: int, b: int):
+        assert r > 0
+        assert P > 0
+        assert b > 0
+        return (P * r * self.lr) <= (b * self.L) and (P * self.u) <= (b * self.U)
---
+++ allocation/test_algorithms.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+__author__ = "Claudio Cicconetti"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import unittest
+from algorithms import ProblemInstance
+
+
+class TestConf(unittest.TestCase):
+    def test_maxP(self):
+        problem = ProblemInstance()
+        self.assertEqual(75000, problem.maxP(10, 4))
+        self.assertEqual(7500, problem.maxP(1, 4))
+        self.assertEqual(300000, problem.maxP(10, 1))
+
+    def test_minP(self):
+        problem = ProblemInstance()
+        self.assertEqual(10, problem.minP(1))
+        self.assertEqual(10, problem.minP(10))
+        self.assertEqual(11, problem.minP(11))
+
+    def test_check_feasible(self):
+        problem = ProblemInstance()
+        self.assertEqual(True, problem.check_feasible(2, 1000, 10))
+        self.assertEqual(True, problem.check_feasible(3, 1000, 15))
+        self.assertEqual(False, problem.check_feasible(2, 1000, 9))
+        self.assertEqual(False, problem.check_feasible(3, 1000, 14))
+
+    def test_broMin(self):
+        problem = ProblemInstance()
+        self.assertEqual((200, 4), problem.broMin(200, 4, 20))
+        self.assertEqual((-1, -1), problem.broMin(200, 400, 20))
+
+    def test_broMax(self):
+        problem = ProblemInstance()
+        self.assertEqual((1000, 20), problem.broMax(200, 4, 20))
+        self.assertEqual((-1, -1), problem.broMax(200, 400, 20))
+
+
+if __name__ == "__main__":
+    unittest.main()
